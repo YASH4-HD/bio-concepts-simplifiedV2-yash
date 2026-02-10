ad53287a04887e1e894a466eca1d9d51ad9f35d8
YASH4-HD <msicc14@gmail.com>
2026-02-11 03:14:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -522,7 +522,24 @@ with tabs[6]:
         with col_right:
             with st.expander("🧪 Protein Translation"):
                 # Simple Translation Logic
-                codon_map = {'ATG':'M','TAA':'_','TAG':'_','TGA':'_','GCA':'A','GCC':'A','GCG':'A','GCT':'A'} # Add more if needed
+               codon_map = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+} # Add more if needed
                 protein = ""
                 for i in range(0, len(raw_seq)-2, 3):
                     codon = raw_seq[i:i+3]
