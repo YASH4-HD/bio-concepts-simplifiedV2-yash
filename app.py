df73e45df57c3bef237d18e4ceb0f578acc53f9d
YASH4-HD <msicc14@gmail.com>
2026-02-11 19:58:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -862,8 +862,6 @@ with tabs[8]:
                 st.code(current_seq, wrap_lines=True)
                 
             st.markdown("</div>", unsafe_allow_html=True)
-
-
     # 4. Footer info
     st.caption("Bio-Nexus Engine v2.4 | Powered by py3Dmol & OpenPDB")
 # =========================
